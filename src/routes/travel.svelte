188ac70f25af928960f8d90626802c7c7c30b3cd
Rafa Dorado <rdr@trifork.com>
2022-11-07 09:20:22 +0100

    content work

--- src/routes/travel.svelte
+++ src/routes/travel.svelte
@@ -1,1 +1,28 @@
-<h1 class="text-4xl font-alice text-center">Travel & Accomodation</h1>
+<script>
+  import { intros } from "svelte/internal";
+
+</script>
+<h1 class="text-4xl font-alice text-center mb-8">Travel & Accomodation</h1>
+
+<h2 class="text-3xl mb-2">Important</h2>
+<p>The wedding will take place in <a class="link" href="https://goo.gl/maps/gCK5VwoqJWJPg2rb9" target="_blank">Can Palleta</a>, a villa in the countryside which you can only get to by car. We recommend staying in the surroundings area.</p>
+
+<h2 class="text-3xl mb-2 mt-4"> 1. Airport </h2>
+<p> To get to Mallorca from outside the island, take a flight to Palma de Mallorca Airport (PMI). There are regular flights from most parts of Europe taking around 2 hours.</p>
+<h2 class="text-3xl mb-2 mt-4"> 2. Getting around </h2>
+<p> The easiest way to travel around Mallorca is by car, you can rent a car directly from the airport with prices starting at 30€ per day. For the best prices, check the Spanish car rental comparison website <a class="link" href="https://www.doyouspain.com/" target="_blank">Rent a car</a>
+<h2 class="text-3xl mb-2 mt-4"> 3. Accommodation </h2>
+<p>We recommend staying in the East side of the island where the wedding will take place. The nearest town is Cala D'or which is just a 10 minute drive away. Cala D'or has plenty of restaurants, supermarkets and beaches all within walking distance.
+    <ul class="list-disc pl-8 mt-2">
+        <li>
+            - Hotels and apartments in Cala D'or <a class="link" href="https://www.booking.com/searchresults.es.html?label=gen173nr-1FCAEoggI46AdIM1gEaEaIAQGYAQq4AQfIAQzYAQHoAQH4AQOIAgGoAgO4ApmCnpsGwAIB0gIkMmIwMmQ0NDUtY2IwYy00OGZjLTkyYTgtMzdlNWU3MTI1Yzcz2AIF4AIB&sid=03fd1f37d0ce08a8b0f59d9895294dce&aid=304142&ss=CAN+PALLETA%2C+S%27Horta%2C+Spain&ssne=Cala+d%27Or&ssne_untouched=Cala+d%27Or&lang=es&src=searchresults&dest_id=ChIJQYgJ-6FRlhIR7lW1druiAwg&dest_type=landmark&place_id=ChIJQYgJ-6FRlhIR7lW1druiAwg&latitude=39.4059375&longitude=3.1984375&ac_position=0&ac_click_type=g&ac_langcode=es&ac_suggestion_list_length=1&search_selected=true&search_pageview_id=af0c4421d52f0458&checkin=2023-06-02&checkout=2023-06-05&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure&nflt=distance%3D5000#map_closed" target="_blank">Booking.com</a>
+  
+        </li>
+        <li>
+            - Villas and large houses in Cala D'or and surrounding areas <a class="link" href="https://www.vrbo.com/en-gb/">HomeAway/Vrbo</a> and <a class="link" href="https://www.fincallorca.com/" target="_blank">Fincallorca</a>
+   
+        </li>
+    </ul>
+    
+<h2 class="text-3xl mb-2  mt-4"> 4 - Transport to the wedding </h2> 
+<p>There is a local taxi company in Cala D-or <a class="link" href="https://www.roig.com/" target="_blank">Autor Roig</a>. If you are staying in Cala D'or and need transport to the wedding we will try to arrange a group bus to make it more cost effective. Please let us know if you need this in the RSVP form.</p>
