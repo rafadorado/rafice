4167c88825a58745e756d6e08ba30b62fa8c77e9
Rafa Dorado <rdr@trifork.com>
2023-01-01 16:51:26 +0000

    Build last changes

---
+++ docs/_app/immutable/pages/faq.svelte-fbe574c9.js
@@ -0,0 +1,71 @@
+import{S as ot,i as nt,s as st,e as t,t as o,k as c,c as n,a as s,h as r,d as a,m as u,b as p,g as rt,K as e,n as _a}from"../chunks/index-351cf2ce.js";import{b as at}from"../chunks/paths-396f020f.js";function lt(tt){let m,l,E,ne,se,k,re,le,y,ie,g,de,he,ce,q,ue,pe,R,me,ge,H,fe,ve,j,be,we,S,ye,xe,z,_e,Pe,J,Ee,ke,W,qe,He,x,Se,f,We,Ce,Ie,C,Te,Ve,Q,Ae,De,I,Me,Fe,B,Le,Ne,K,Re,i,T,je,ze,V,Je,Qe,_,Be,v,Ke,Ue,Ye,A,Ge,Oe,U,Xe,Ze,D,$e,ea,Y,aa,ta,M,oa,na,G,sa,ra,O,la,ia,F,da,ha,P,ca,b,ua,pa,ma,L,ga,fa,X,va,ba,N,wa,ya,Z,xa;return{c(){m=t("div"),l=t("div"),E=t("h1"),ne=o("Questions & Answers"),se=c(),k=t("h2"),re=o("Where is the wedding taking place?"),le=c(),y=t("p"),ie=o(`Both the wedding ceremony and the celebrations will take place the same
+      location in `),g=t("a"),de=o("Can Palleta"),he=o(`, S'horta, Felanitx, Mallorca. Can Palleta is a villa with a garden and
+      pool, everything will take place outdoors.`),ce=c(),q=t("h2"),ue=o("What will the weather be like?"),pe=c(),R=t("p"),me=o(`The weather is usually quite warm in early June in with temperatures
+      between 20 and 30 degrees celcius. The wedding starts later in the
+      afternoon to avoid the hottest part of the day. Don't forget to bring
+      suncream, hats and sunglasses.`),ge=c(),H=t("h2"),fe=o("What is the dress code?"),ve=c(),j=t("p"),be=o(`You can wear whatever you feel comfortable in to our wedding, there is no
+      dress code! As the weather will be warm, you should wear summer clothes
+      and you may need a light jacket for the evening.`),we=c(),S=t("h2"),ye=o("Can I bring a plus 1?"),xe=c(),z=t("p"),_e=o(`We are looking forward to sharing this special day with the people we care
+      about. Unfortunately we are limited by space and cost and cannot offer all
+      our guests a plus 1. Please do not bring a plus 1 to our wedding if we
+      have not invited them ourselves.`),Pe=c(),J=t("p"),Ee=o(`This is an adults wedding. For those travelling from outside of Mallorca,
+      of course the little ones are invited. Please let us know if you will be
+      bringing any children in the RSVP form.`),ke=c(),W=t("h2"),qe=o("What food and drink will be at the wedding?"),He=c(),x=t("p"),Se=o(`We will have plenty of food and drinks throughout the event, see the full
+      schedule `),f=t("a"),We=o("here"),Ce=o(`.
+      We will have a mixture of vegan, vegetarian and meat options. If you have
+      any dietry requirements or allergies you can let us know in the RSVP form.`),Ie=c(),C=t("h2"),Te=o("Is there a gifts registry?"),Ve=c(),Q=t("p"),Ae=o(`As cheesy as it sounds, your presence at our wedding is a present in
+      itself, we don't expect any gifts. However, we will have a letterbox for
+      wedding cards at the ceremony. If you want to give us a little something
+      for our honeymoon, we will be adding a link to this webpage later on for
+      you to contribute online.`),De=c(),I=t("h2"),Me=o("Can we park at the wedding venue?"),Fe=c(),B=t("p"),Le=o(`There will be parking spaces at the wedding venue. We will be putting up
+      signs near the entrance to the venue to direct you. If you plan to park at
+      the venue, please let us know in the RSVP form.`),Ne=c(),K=t("div"),Re=c(),i=t("div"),T=t("h1"),je=o("Preguntas Frecuentes"),ze=c(),V=t("h2"),Je=o("\xBFD\xF3nde se celebrar\xE1 la boda?"),Qe=c(),_=t("p"),Be=o("La ceremonia y la fiesta ser\xE1 en "),v=t("a"),Ke=o("Can Palleta"),Ue=o(", S'horta, Felanitx, Mallorca. La boda ser\xE1 en el exterior"),Ye=c(),A=t("h2"),Ge=o("\xBFQu\xE9 tipo de tiempo hace?"),Oe=c(),U=t("p"),Xe=o(`Esperamos que haga buen tiempo, en general en Mallorca solemos tener unos
+      20 grados de media. La boda ser\xE1 por la tarde y as\xED evitamos las horas m\xE1s
+      calientes del d\xEDa.`),Ze=c(),D=t("h2"),$e=o("\xBFC\xF3mo ir vestido?"),ea=c(),Y=t("p"),aa=o(`No tenemos ning\xFAn c\xF3digo de vestimenta, ven c\xF3mo te apetezca :-)
+      Recomendamos traer alguna chaquetilla por si refresca en la noche.`),ta=c(),M=t("h2"),oa=o("\xBFPuedo traer alg\xFAn amigo?"),na=c(),G=t("p"),sa=o(`Nos encantar\xEDa invitar a todo el mundo, pero estamos limitados por el
+      espacio y los costes. Por favor, no traigas a invitados a no ser que los
+      hayamos invitado nosotros.`),ra=c(),O=t("p"),la=o(`Es una boda de adultos, pero si no tienes d\xF3nde dejar a los ni\xF1os no hay
+      problema, tienen espacio para jugar en la villa. Por favor av\xEDsanos en el
+      formulario de asistencia si vienes con peque\xF1os :-)`),ia=c(),F=t("h2"),da=o("\xBFQu\xE9 comida y bebida habr\xE1?"),ha=c(),P=t("p"),ca=o(`Nosotros ponemos la comida y bebida para toda la tarde/noche. Habr\xE1 barra
+      libre hasta la 1:30. Por favor comprueba la agenda `),b=t("a"),ua=o("aqu\xED"),pa=o(`. Tendremos una mezcla de vegano/vegatariano y carnes :-). Si tienes
+      alguna alergia o algo que tengamos que avisar al catering por favor
+      av\xEDsanos en el formulario de asistencia.`),ma=c(),L=t("h2"),ga=o("\xBFTengo que hacer alg\xFAn regalo?"),fa=c(),X=t("p"),va=o(`El regalo es que pod\xE1is acompa\xF1arnos :-). No esperamos nada. De todas
+      formas, si a\xFAn as\xED quer\xE9is regalarnos algo, aceptamos sobres a lo
+      B\xE1rcenas. Pondremos un link para que nos ayud\xE9is en nuestra Luna de Miel.`),ba=c(),N=t("h2"),wa=o("\xBFHay Parking?"),ya=c(),Z=t("p"),xa=o(`S\xED, tendremos una zona habilitada y lo indicaremos. Somos bastantes as\xED
+      que por favor, si necesit\xE1is parking avisadnos en el formulario de
+      asistencia .`),this.h()},l($){m=n($,"DIV",{class:!0});var w=s(m);l=n(w,"DIV",{class:!0});var d=s(l);E=n(d,"H1",{class:!0});var Pa=s(E);ne=r(Pa,"Questions & Answers"),Pa.forEach(a),se=u(d),k=n(d,"H2",{class:!0});var Ea=s(k);re=r(Ea,"Where is the wedding taking place?"),Ea.forEach(a),le=u(d),y=n(d,"P",{});var ee=s(y);ie=r(ee,`Both the wedding ceremony and the celebrations will take place the same
+      location in `),g=n(ee,"A",{class:!0,href:!0,target:!0});var ka=s(g);de=r(ka,"Can Palleta"),ka.forEach(a),he=r(ee,`, S'horta, Felanitx, Mallorca. Can Palleta is a villa with a garden and
+      pool, everything will take place outdoors.`),ee.forEach(a),ce=u(d),q=n(d,"H2",{class:!0});var qa=s(q);ue=r(qa,"What will the weather be like?"),qa.forEach(a),pe=u(d),R=n(d,"P",{});var Ha=s(R);me=r(Ha,`The weather is usually quite warm in early June in with temperatures
+      between 20 and 30 degrees celcius. The wedding starts later in the
+      afternoon to avoid the hottest part of the day. Don't forget to bring
+      suncream, hats and sunglasses.`),Ha.forEach(a),ge=u(d),H=n(d,"H2",{class:!0});var Sa=s(H);fe=r(Sa,"What is the dress code?"),Sa.forEach(a),ve=u(d),j=n(d,"P",{});var Wa=s(j);be=r(Wa,`You can wear whatever you feel comfortable in to our wedding, there is no
+      dress code! As the weather will be warm, you should wear summer clothes
+      and you may need a light jacket for the evening.`),Wa.forEach(a),we=u(d),S=n(d,"H2",{class:!0});var Ca=s(S);ye=r(Ca,"Can I bring a plus 1?"),Ca.forEach(a),xe=u(d),z=n(d,"P",{});var Ia=s(z);_e=r(Ia,`We are looking forward to sharing this special day with the people we care
+      about. Unfortunately we are limited by space and cost and cannot offer all
+      our guests a plus 1. Please do not bring a plus 1 to our wedding if we
+      have not invited them ourselves.`),Ia.forEach(a),Pe=u(d),J=n(d,"P",{});var Ta=s(J);Ee=r(Ta,`This is an adults wedding. For those travelling from outside of Mallorca,
+      of course the little ones are invited. Please let us know if you will be
+      bringing any children in the RSVP form.`),Ta.forEach(a),ke=u(d),W=n(d,"H2",{class:!0});var Va=s(W);qe=r(Va,"What food and drink will be at the wedding?"),Va.forEach(a),He=u(d),x=n(d,"P",{});var ae=s(x);Se=r(ae,`We will have plenty of food and drinks throughout the event, see the full
+      schedule `),f=n(ae,"A",{class:!0,href:!0,target:!0});var Aa=s(f);We=r(Aa,"here"),Aa.forEach(a),Ce=r(ae,`.
+      We will have a mixture of vegan, vegetarian and meat options. If you have
+      any dietry requirements or allergies you can let us know in the RSVP form.`),ae.forEach(a),Ie=u(d),C=n(d,"H2",{class:!0});var Da=s(C);Te=r(Da,"Is there a gifts registry?"),Da.forEach(a),Ve=u(d),Q=n(d,"P",{});var Ma=s(Q);Ae=r(Ma,`As cheesy as it sounds, your presence at our wedding is a present in
+      itself, we don't expect any gifts. However, we will have a letterbox for
+      wedding cards at the ceremony. If you want to give us a little something
+      for our honeymoon, we will be adding a link to this webpage later on for
+      you to contribute online.`),Ma.forEach(a),De=u(d),I=n(d,"H2",{class:!0});var Fa=s(I);Me=r(Fa,"Can we park at the wedding venue?"),Fa.forEach(a),Fe=u(d),B=n(d,"P",{});var La=s(B);Le=r(La,`There will be parking spaces at the wedding venue. We will be putting up
+      signs near the entrance to the venue to direct you. If you plan to park at
+      the venue, please let us know in the RSVP form.`),La.forEach(a),d.forEach(a),Ne=u(w),K=n(w,"DIV",{class:!0}),s(K).forEach(a),Re=u(w),i=n(w,"DIV",{class:!0});var h=s(i);T=n(h,"H1",{class:!0});var Na=s(T);je=r(Na,"Preguntas Frecuentes"),Na.forEach(a),ze=u(h),V=n(h,"H2",{class:!0});var Ra=s(V);Je=r(Ra,"\xBFD\xF3nde se celebrar\xE1 la boda?"),Ra.forEach(a),Qe=u(h),_=n(h,"P",{});var te=s(_);Be=r(te,"La ceremonia y la fiesta ser\xE1 en "),v=n(te,"A",{class:!0,href:!0,target:!0});var ja=s(v);Ke=r(ja,"Can Palleta"),ja.forEach(a),Ue=r(te,", S'horta, Felanitx, Mallorca. La boda ser\xE1 en el exterior"),te.forEach(a),Ye=u(h),A=n(h,"H2",{class:!0});var za=s(A);Ge=r(za,"\xBFQu\xE9 tipo de tiempo hace?"),za.forEach(a),Oe=u(h),U=n(h,"P",{});var Ja=s(U);Xe=r(Ja,`Esperamos que haga buen tiempo, en general en Mallorca solemos tener unos
+      20 grados de media. La boda ser\xE1 por la tarde y as\xED evitamos las horas m\xE1s
+      calientes del d\xEDa.`),Ja.forEach(a),Ze=u(h),D=n(h,"H2",{class:!0});var Qa=s(D);$e=r(Qa,"\xBFC\xF3mo ir vestido?"),Qa.forEach(a),ea=u(h),Y=n(h,"P",{});var Ba=s(Y);aa=r(Ba,`No tenemos ning\xFAn c\xF3digo de vestimenta, ven c\xF3mo te apetezca :-)
+      Recomendamos traer alguna chaquetilla por si refresca en la noche.`),Ba.forEach(a),ta=u(h),M=n(h,"H2",{class:!0});var Ka=s(M);oa=r(Ka,"\xBFPuedo traer alg\xFAn amigo?"),Ka.forEach(a),na=u(h),G=n(h,"P",{});var Ua=s(G);sa=r(Ua,`Nos encantar\xEDa invitar a todo el mundo, pero estamos limitados por el
+      espacio y los costes. Por favor, no traigas a invitados a no ser que los
+      hayamos invitado nosotros.`),Ua.forEach(a),ra=u(h),O=n(h,"P",{});var Ya=s(O);la=r(Ya,`Es una boda de adultos, pero si no tienes d\xF3nde dejar a los ni\xF1os no hay
+      problema, tienen espacio para jugar en la villa. Por favor av\xEDsanos en el
+      formulario de asistencia si vienes con peque\xF1os :-)`),Ya.forEach(a),ia=u(h),F=n(h,"H2",{class:!0});var Ga=s(F);da=r(Ga,"\xBFQu\xE9 comida y bebida habr\xE1?"),Ga.forEach(a),ha=u(h),P=n(h,"P",{});var oe=s(P);ca=r(oe,`Nosotros ponemos la comida y bebida para toda la tarde/noche. Habr\xE1 barra
+      libre hasta la 1:30. Por favor comprueba la agenda `),b=n(oe,"A",{class:!0,href:!0,target:!0});var Oa=s(b);ua=r(Oa,"aqu\xED"),Oa.forEach(a),pa=r(oe,`. Tendremos una mezcla de vegano/vegatariano y carnes :-). Si tienes
+      alguna alergia o algo que tengamos que avisar al catering por favor
+      av\xEDsanos en el formulario de asistencia.`),oe.forEach(a),ma=u(h),L=n(h,"H2",{class:!0});var Xa=s(L);ga=r(Xa,"\xBFTengo que hacer alg\xFAn regalo?"),Xa.forEach(a),fa=u(h),X=n(h,"P",{});var Za=s(X);va=r(Za,`El regalo es que pod\xE1is acompa\xF1arnos :-). No esperamos nada. De todas
+      formas, si a\xFAn as\xED quer\xE9is regalarnos algo, aceptamos sobres a lo
+      B\xE1rcenas. Pondremos un link para que nos ayud\xE9is en nuestra Luna de Miel.`),Za.forEach(a),ba=u(h),N=n(h,"H2",{class:!0});var $a=s(N);wa=r($a,"\xBFHay Parking?"),$a.forEach(a),ya=u(h),Z=n(h,"P",{});var et=s(Z);xa=r(et,`S\xED, tendremos una zona habilitada y lo indicaremos. Somos bastantes as\xED
+      que por favor, si necesit\xE1is parking avisadnos en el formulario de
+      asistencia .`),et.forEach(a),h.forEach(a),w.forEach(a),this.h()},h(){p(E,"class","text-4xl font-alice text-center mb-8"),p(k,"class","text-3xl mb-2"),p(g,"class","link"),p(g,"href","https://goo.gl/maps/gCK5VwoqJWJPg2rb9"),p(g,"target","_blank"),p(q,"class","text-3xl mb-2 mt-4"),p(H,"class","text-3xl mb-2 mt-4"),p(S,"class","text-3xl mb-2 mt-4"),p(W,"class","text-3xl mb-2 mt-4"),p(f,"class","link"),p(f,"href",at+"/schedule"),p(f,"target","_blank"),p(C,"class","text-3xl mb-2 mt-4"),p(I,"class","text-3xl mb-2 mt-4"),p(l,"class","flex flex-col p-8 md:p-0"),p(K,"class","border-2 border-primary md:hidden"),p(T,"class","text-4xl font-alice text-center mb-8"),p(V,"class","text-3xl mb-2"),p(v,"class","link"),p(v,"href","https://goo.gl/maps/gCK5VwoqJWJPg2rb9"),p(v,"target","_blank"),p(A,"class","text-3xl mb-2 mt-4"),p(D,"class","text-3xl mb-2 mt-4"),p(M,"class","text-3xl mb-2 mt-4"),p(F,"class","text-3xl mb-2 mt-4"),p(b,"class","link"),p(b,"href",at+"/schedule"),p(b,"target","_blank"),p(L,"class","text-3xl mb-2 mt-4"),p(N,"class","text-3xl mb-2 mt-4"),p(i,"class","flex flex-col p-8 md:p-0"),p(m,"class","grid md:grid-cols-2 gap-x-8")},m($,w){rt($,m,w),e(m,l),e(l,E),e(E,ne),e(l,se),e(l,k),e(k,re),e(l,le),e(l,y),e(y,ie),e(y,g),e(g,de),e(y,he),e(l,ce),e(l,q),e(q,ue),e(l,pe),e(l,R),e(R,me),e(l,ge),e(l,H),e(H,fe),e(l,ve),e(l,j),e(j,be),e(l,we),e(l,S),e(S,ye),e(l,xe),e(l,z),e(z,_e),e(l,Pe),e(l,J),e(J,Ee),e(l,ke),e(l,W),e(W,qe),e(l,He),e(l,x),e(x,Se),e(x,f),e(f,We),e(x,Ce),e(l,Ie),e(l,C),e(C,Te),e(l,Ve),e(l,Q),e(Q,Ae),e(l,De),e(l,I),e(I,Me),e(l,Fe),e(l,B),e(B,Le),e(m,Ne),e(m,K),e(m,Re),e(m,i),e(i,T),e(T,je),e(i,ze),e(i,V),e(V,Je),e(i,Qe),e(i,_),e(_,Be),e(_,v),e(v,Ke),e(_,Ue),e(i,Ye),e(i,A),e(A,Ge),e(i,Oe),e(i,U),e(U,Xe),e(i,Ze),e(i,D),e(D,$e),e(i,ea),e(i,Y),e(Y,aa),e(i,ta),e(i,M),e(M,oa),e(i,na),e(i,G),e(G,sa),e(i,ra),e(i,O),e(O,la),e(i,ia),e(i,F),e(F,da),e(i,ha),e(i,P),e(P,ca),e(P,b),e(b,ua),e(P,pa),e(i,ma),e(i,L),e(L,ga),e(i,fa),e(i,X),e(X,va),e(i,ba),e(i,N),e(N,wa),e(i,ya),e(i,Z),e(Z,xa)},p:_a,i:_a,o:_a,d($){$&&a(m)}}}class ht extends ot{constructor(m){super(),nt(this,m,null,lt,st,{})}}export{ht as default};
