1c00e32ffee115ee029ba313aba583fd19f518c9
Rafa Dorado <rdr@trifork.com>
2023-01-07 14:01:43 +0100

    rsvp

--- src/routes/rsvp.svelte
+++ src/routes/rsvp.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col mt-16 p-8">
-    <h1 class="text-2xl text-center"> Please, confirm your assitance and fill this form with your preferences</h1>
+    <h1 class="text-2xl text-center"> Please, confirm your assitance by filling out this form by <strong>30th April</strong>. You can tell us about any special requirements in the form.</h1>
     <hr class="w-1/2 border-b my-4 self-center">
-    <h1 class="text-2xl text-center "> Por favor, confirma tu asistencia y rellena el formulario con tus preferencias</h1>
+    <h1 class="text-2xl text-center "> Por favor, confirma tu asistencia antes del <strong>30 de abril</strong> y rellena el formulario con tus preferencias. </h1>
     <div class="flex items-center justify-center mt-16">
         <a href="https://forms.gle/fgrFn7zUzhh9mC15A" class="btn-primary" target="_blank"> RSVP / Confirma Asistencia </a>
     </div>
